0c7ed4a82c98f118f565f7c3c6ffd4b63385dfef
melampyge <ozer.duman@gmail.com>
2017-07-23 00:01:15 +0200

    Updating the cpp module.

--- Analysis/AnalyseNeighbours.cpp
+++ Analysis/AnalyseNeighbours.cpp
@@ -19,11 +19,12 @@ AnalyseNeighbours::AnalyseNeighbours (T sim_, Beads *beads_) {
 
 AnalyseNeighbours::~AnalyseNeighbours () { }
 
-double AnalyseNeighbours::perform_analysis () {
+tuple<double, double> AnalyseNeighbours::perform_analysis () {
   
-  double avg_num_neigh = calc_num_neighbours(beads->x, beads->y);
+  double avg_num_neigh, avg_num_surface_cells;
+  tie(avg_num_neigh, avg_num_surface_cells) = calc_num_neighbours(x, y, sim);
   
-  return avg_num_neigh;
+  return make_tuple(avg_num_neigh, avg_num_surface_cells);
 }
 
 void AnalyseNeighbours::write_analysis_results (double data, string outfilepath) {
@@ -41,9 +42,12 @@ void AnalyseNeighbours::build_linked_cell_list(const double * const *x,
                                                const double * const *y,
                                                vector<int> & heads,
                                                vector<int> & llist,
-                                               double wbin, int nboxes, int nsize,
-                                               int step, int natoms, double l) {
-  /* build a linked cell/bucket list */
+                                               const double wbin, const int nboxes,
+                                               const int nsize, const int step,
+                                               const int natoms, const double l) {
+  /* build a linked cell/box list */
+  
+  cout << "Building linked cell list" << endl;
   
   for (int j = 0; j < natoms; j++) {
     int xbin = get_bin_number(get_single_img_pos(x[step][j], l), wbin, nboxes);
@@ -59,11 +63,129 @@ void AnalyseNeighbours::build_linked_cell_list(const double * const *x,
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-double AnalyseNeighbours::calc_num_neighbours (const double * const *x,
-                                               const double * const *y) {
-  /* calculate densities per bin per frame */
+set<int> AnalyseNeighbours::get_neighs_of_a_cell (const int start, const int end,
+                                                 const double * const *x,
+                                                 const double * const *y,
+                                                 const int step, const double rcut,
+                                                 const int nboxes,
+                                                 const double lx, const double ly,
+                                                 const vector<int> & cid,
+                                                 const vector<int> & heads,
+                                                 const vector<int> & llist) {
+  /* get the neighbours of a single cell at a given timestep */
+  
+  // set variables related to the analysis
+  
+  set<int> neighs;
+  const double rth2 = rcut*rcut;
+  
+  // run over each bead of the cell
+  
+  for (int j = start; j < end; j++) {
+    int host_cell_id = cid[j];
+    
+    int xbin = get_bin_number(get_single_img_pos(x[step][j], lx), rcut, nboxes);
+    int ybin = get_bin_number(get_single_img_pos(y[step][j], ly), rcut, nboxes);
+    
+    // loop over the neighbouring bins
+    
+    for (int ix = -1; ix < 2; ix++) {
+      int xneighbin = wrap_to_range(ix+xbin, nboxes);
+      
+      for (int iy = -1; iy < 2; iy++) {
+        int yneighbin = wrap_to_range(iy+ybin, nboxes);
+        int neighbin = xneighbin*nboxes + yneighbin;
+        
+        // restore the head bead
+        
+        int idx = heads[neighbin];
+        
+        // traverse the neighbouring bin
+        
+        while (idx != -1) {
+          
+          // calculate the distance between the beads
+          // if they do not belong to the same cell
+          
+          int neigh_cell_id = cid[idx];
+          if (host_cell_id != neigh_cell_id) {
+            
+            double dx = x[step][idx] - x[step][j];
+            dx = get_min_img_dist(dx, lx);
+            double dy = y[step][idx] - y[step][j];
+            dy = get_min_img_dist(dy, ly);
+            
+            if (dx*dx + dy*dy < rth2)
+              neighs.insert(neigh_cell_id);
+            
+          }
+          
+          // traverse the linked list
+          
+          idx = llist[idx];
+          
+        }     // inside neighbouring bin linked list
+        
+      }       // y neighbour bins loop
+    }         // x neighbour bins loop
+  }           // bead loop
+  
+  return neighs;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+map<int, set<int> > AnalyseNeighbours::build_neigh_list (const double * const *x,
+                                                         const double * const *y,
+                                                         const vector<int> & cid,
+                                                         const vector<int> & nbpp,
+                                                         const vector<int> & heads,
+                                                         const vector<int> & llist,
+                                                         const int npols,
+                                                         const double rcut,
+                                                         const int nsteps,
+                                                         const int step,
+                                                         const int nboxes,
+                                                         const double lx,
+                                                         const double ly) {
+  /* build the neighbour list of all cells at a given timestep */
+  
+  cout << "Building neighbour list" << endl;
+  
+  map<int, set<int> > neigh_list;
+  
+  int bead_idx = 0;
+  for (int n = 0; n < npols; n++) {
+    
+    // iterate over the beads of the cell and get the neighbours of the cell
+    
+    int last_bead_idx = bead_idx + nbpp[n];
+    set<int> neighs_of_this_cell = get_neighs_of_a_cell(bead_idx, last_bead_idx,
+                                                        x, y,
+                                                        step, rcut, nboxes, lx, ly,
+                                                        cid, heads, llist);
+    bead_idx = last_bead_idx;
+    
+    // add the neighbours of this cell to the neighbour list
+    
+    neigh_list.insert(pair <int, set<int> > (n, neighs_of_this_cell));
+    
+  }             // cell loop
+  
+  
+  return neigh_list;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+tuple<double, double> AnalyseNeighbours::calc_num_neighbours (const double * const *x,
+                                                              const double * const *y) {
+  /* calculate number of neighbours per bin per frame */
+  
+  // set parameters related to the analysis
   
   double avg_num_neigh = 0.;
+  double avg_num_surface_cells = 0.;
   
   // set linked cell/box list parameters
   
@@ -72,51 +194,47 @@ double AnalyseNeighbours::calc_num_neighbours (const double * const *x,
   int nsize = nboxes*nboxes;
   rcut = sim.lx/nboxes;
   vector<int> heads(nsize, -1);
-  vector<int> llist(sim.nbeads*100, -1);
-  
-  vector<int> num_neighs_per_cell(sim.ncells, 0);
+  vector<int> llist(sim.nbeads, -1);
   
   for (int step = 0; step < sim.nsteps; step++) {
     cout << "step / nsteps : " << step << " / " << sim.nsteps << endl;
     
     // build the linked cell/box list
     
+    vector<int> heads(nsize, -1);
+    vector<int> llist(sim.nbeads, -1);
     build_linked_cell_list(x, y, heads, llist,
                            rcut, nboxes, nsize,
                            step, sim.nbeads, sim.lx);
     
-    // count the number of neighbours of each cell
+    // build the neighbour list
     
-    vector<int> num_neighs_per_cell_per_time(sim.ncells, 0);
-    int k = 0;
-    for (int n = 0; n < sim.ncells; n++) {
-      for (int j = 0; j < sim.nbpc[j]; j++) {
-        int xbin = get_bin_number(get_single_img_pos(x[step][k], sim.lx), rcut, nboxes);
-        int ybin = get_bin_number(get_single_img_pos(y[step][k], sim.ly), rcut, nboxes);
-        //int bin = xbin*nboxes + ybin;
-        
-        // loop over the neighbouring bins
-        
-        for (int ix = -1; ix < 2; ix++) {
-          int xneighbin = (ix+xbin)%nboxes;
-          
-          for (int iy = -1; iy < 2; iy++) {
-            int yneighbin = (iy+ybin)%nboxes;
-            int neighbin = xneighbin*nboxes + yneighbin;
-            
-            // restore the head bead
-            
-            
-            
-          }       // y neighbour bin loop
-        }         // x neighbor bin loop
-        k++;
-      }           // nbpc loop
-    }             // cell loop
+    map<int, set<int> > neigh_list = build_neigh_list(x, y, sim.cid, sim.nbpp,
+                                                      heads, llist,
+                                                      sim.npols, rcut,
+                                                      sim.nsteps, step,
+                                                      nboxes, sim.lx, sim.ly);
+    
+    // count the average number of neighbours of cells
+    
+    double avg_num_neigh_per_cell = 0.;
+    for (const auto& kv : neigh_list) {
+      int num_neighs = kv.second.size();
+      avg_num_neigh_per_cell += num_neighs;  
+      if (num_neighs < 4)
+        avg_num_surface_cells++;
+    }
+    avg_num_neigh_per_cell /= neigh_list.size();
+    avg_num_neigh += avg_num_neigh_per_cell;
+    avg_num_surface_cells /= neigh_list.size();
     
   }               // timestep loop
   
-  return avg_num_neigh;
+  avg_num_neigh /= sim.nsteps;
+  avg_num_surface_cells /= sim.nsteps;
+  
+  
+  return make_tuple(avg_num_neigh, avg_num_surface_cells);
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
--- Analysis/AnalyseNeighbours.hpp
+++ Analysis/AnalyseNeighbours.hpp
@@ -18,6 +18,7 @@
 #include <set>
 #include <stack>
 #include <algorithm>
+#include <tuple>
 
 #include "data_structures.hpp"
 #include "basic.hpp"
@@ -34,19 +35,40 @@ public:
   
   AnalyseNeighbours(T sim_, Beads *beads_);
   ~AnalyseNeighbours();
-  double perform_analysis();
+  tuple<double, double> AnalyseNeighbours::perform_analysis ();
   void write_analysis_results(double data, string outfilepath);
 
   void AnalyseNeighbours::build_linked_cell_list(const double * const *x,
                                                  const double * const *y,
-                                                 std::vector<int> & heads,
-                                                 std::vector<int> & llist,
-                                                 double wbin, int nboxes, int nsize,
-                                                 int step, int natoms, double l);
- 
-  double AnalyseNeighbours::calc_num_neighbours (const double * const *x,
-                                                 const double * const *y);
-  
+                                                 vector<int> & heads,
+                                                 vector<int> & llist,
+                                                 const double wbin, const int nboxes,
+                                                 const int nsize, const int step,
+                                                 const int natoms, const double l);
+  set<int> AnalyseNeighbours::get_neighs_of_a_cell (const int start, const int end,
+                                                    const double * const *x,
+                                                    const double * const *y,
+                                                    const int step, const double rcut,
+                                                    const int nboxes,
+                                                    const double lx, const double ly,
+                                                    const vector<int> & cid,
+                                                    const vector<int> & heads,
+                                                    const vector<int> & llist);
+  map<int, set<int> > AnalyseNeighbours::build_neigh_list (const double * const *x,
+                                                           const double * const *y,
+                                                           const vector<int> & cid,
+                                                           const vector<int> & nbpp,
+                                                           const vector<int> & heads,
+                                                           const vector<int> & llist,
+                                                           const int npols,
+                                                           const double rcut,
+                                                           const int nsteps,
+                                                           const int step,
+                                                           const int nboxes,
+                                                           const double lx,
+                                                           const double ly);
+  tuple<double, double> AnalyseNeighbours::calc_num_neighbours (const double * const *x,
+                                                                const double * const *y);
 };
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
