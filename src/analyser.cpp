7870ae1199f5a7f0801ea632de5696a5406b0053
melampyge <ozer.duman@gmail.com>
2017-07-28 17:53:20 +0200

    First cpp version to be shipped.

--- src/analyser.cpp
+++ src/analyser.cpp
@@ -7,46 +7,64 @@
 
 using namespace std;
 
-
 /////////////////////////////////////////////////////////////////////////////////
 
 int main (int argc, char *argv[]) {
   /* argv[1] -> datafilename         (path of the data file)
-     argv[2] -> beadsorpols          (make a choice between reading beads or polymers)
-     argv[3] -> filsorcells          (make a choice of sim type between filaments or cells)
-     argv[4...] -> outfilename(s)... (path of the analysis results file)
+     argv[2] -> analysisname 	     (name of the analysis type)
+     argv[3] -> beadsorpols          (make a choice between reading beads or polymers)
+     argv[4] -> filsorcells          (make a choice of sim type between filaments or cells)
+     argv[5...] -> outfilename(s)... (path of the analysis results file)
    */
   
   // get command line arguments
   
   char *datafilename = argv[1];
-  char *beadsorpols = argv[2];
-  char *filsorcells = argv[3];
-  char *outfilename = argv[4];
-  //char *outfilename_2 = argv[5];
+  char *analysisname = argv[2];
+  char *beadsorpols = argv[3];
+  char *filsorcells = argv[4];
+  char *outfilename = argv[5];
   
   cout << "Data file: " << datafilename << endl;
+  cout << "Analysis type: " << analysisname << endl;
   cout << "Analysis to be conducted on: " << beadsorpols << endl;
   cout << "Simulation type: " << filsorcells << endl;
   cout << "Analysis results output file path: " << outfilename << endl;
-  //cout << "Analysis results output file path: " << outfilename_2 << endl;
   
-  // perform the analysis
+  // choose analysis type
   
-//   AnalyseNeighbours analysis(datafilename, filsorcells);
-/*  AnalysePairCorr analysis(datafilename, filsorcells);*/
-/*  AnalyseStaticStruct analysis(datafilename, filsorcells); */
-/*  AnalyseLatticeOrder analysis(datafilename, filsorcells);*/
-//   AnalyseAreaDiff analysis(datafilename, filsorcells);
-  AnalyseDensityHistogram analysis(datafilename, filsorcells);
-
-  analysis.perform_analysis();
-  
-  
-  // write the computed results
-  
-  //analysis.write_analysis_results(outfilename, outfilename_2);
-  analysis.write_analysis_results(outfilename);
+  if (strcmp(analysisname, "Num_neighbour") == 0) {   
+    char *outfilename_2 = argv[6];  
+    cout << "Analysis results output file path: " << outfilename_2 << endl;
+    AnalyseNeighbours analysis(datafilename, filsorcells);
+    analysis.perform_analysis();    
+    analysis.write_analysis_results(outfilename, outfilename_2);
+  }
+  else if (strcmp(analysisname, "Static_struct") == 0) { 	
+    AnalyseStaticStruct analysis(datafilename, filsorcells);
+    analysis.perform_analysis();  
+    analysis.write_analysis_results(outfilename);    
+  }
+  else if (strcmp(analysisname, "Pair_corr") == 0) {
+    AnalysePairCorr analysis(datafilename, filsorcells);
+    analysis.perform_analysis();  
+    analysis.write_analysis_results(outfilename);     
+  }
+  else if (strcmp(analysisname, "Lattice_order") == 0) {
+    AnalyseLatticeOrder analysis(datafilename, filsorcells);
+    analysis.perform_analysis();  
+    analysis.write_analysis_results(outfilename);      
+  }  
+  else if (strcmp(analysisname, "Area_diff") == 0) {
+    AnalyseAreaDiff analysis(datafilename, filsorcells);
+    analysis.perform_analysis();  
+    analysis.write_analysis_results(outfilename);      
+  }  
+  else if (strcmp(analysisname, "Density_histo") == 0) {
+    AnalyseDensityHistogram analysis(datafilename, filsorcells);
+    analysis.perform_analysis();  
+    analysis.write_analysis_results(outfilename);      
+  }    
   
   return 0;
 }
